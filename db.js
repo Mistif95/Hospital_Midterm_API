eec527f058ce8996f15d85fdb806074dc0ebcd36
Rayhan Aliffio Wibowo <125962892+Mistif95@users.noreply.github.com>
2026-04-15 19:23:24 +0700

    MySQL connection pool

---
+++ db.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+// Mysql connection pool
+const pool = mysql.createPool({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'hospital_assets',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+// promise wrapper test later
+module.exports = pool.promise();
